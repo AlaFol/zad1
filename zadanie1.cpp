7e45e41d7bc81ce488c96ec633133c816d312858
AlaFol <alafolwarcznyde@gmail.com>
2024-12-16 15:04:06 +0100

    Create zadanie1.cpp
    c++

---
+++ zadanie1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+count<<"witamy w programiec++"<<endl;
+count<<"poznajemy zasady programowania"<<endl;
+count<<"pozanjemy zasady programowania/n<<;
+
+cout<<"wynik wyrażenia 123*158-5 wynosi:"<< endl;
+cout<<123*158-5<< endl;
+}
